732a7c1ad85fe095bb14a0bef2150125e62d7f2f
Volodia Chornenkiy <chornenkyy@gmail.com>
2015-09-06 23:47:37 +0300

    init commit
    servlets doesn`t work

---
+++ src/java/ua/com/okam1/AnnouncementPlugin.java
@@ -0,0 +1,22 @@
+package ua.com.okam1;
+
+import java.io.File;
+
+import org.jivesoftware.openfire.container.Plugin;
+import org.jivesoftware.openfire.container.PluginManager;
+
+public class AnnouncementPlugin implements Plugin {
+
+	@Override
+	public void initializePlugin(PluginManager arg0, File arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void destroyPlugin() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
